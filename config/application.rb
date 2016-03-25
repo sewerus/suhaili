a384836dca22dce587fc3bf5f5a5fdeb53ecfd72
Seweryn <seweryn606@gmail.com>
2016-03-25 11:52:40 +0100

    Posts controller

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,6 @@ module Suhaili
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.autoload_paths += %W(#{config.root}/lib)
 
   end
 end
