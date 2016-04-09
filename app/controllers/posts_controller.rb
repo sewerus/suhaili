bff861257d9170837abb6add56a2f62c7bb15e99
sewerus <seweryn606@gmail.com>
2016-04-09 20:00:55 +0200

    posts#2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(posts_params)
+    @post = Post.new(post_params)
     if @post.save
       redirect_to '/posts'
     else
