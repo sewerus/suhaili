8220d1f34c8c4e2620e4ec207f5e9ba165fc6342
sewerus <seweryn606@gmail.com>
2016-04-09 23:07:57 +0200

    contact#4

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,44 @@
+class ContactsController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :new, :delete]
+  def index
+    @contacts = Contact.all
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    redirect_to '/contacts'
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to '/contacts'
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update_attributes(contact_params)
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
+
+  private
+  def contact_params
+    params.require(:contact).permit(:content)
+  end
+end
