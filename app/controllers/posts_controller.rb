337c726a2939a9e33392bbff3b29d8e0b4b0908b
sewerus <seweryn606@gmail.com>
2016-04-10 13:21:38 +0200

    Posts #2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-    redirect_to root_path
+    redirect_to '/posts'
   end
 
   def new
@@ -30,7 +30,7 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     if @post.update_attributes(post_params)
-      redirect_to root_path
+      redirect_to '/posts'
     else
       render 'edit'
     end
