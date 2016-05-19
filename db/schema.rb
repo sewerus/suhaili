4e3ee875820cc32aa65b1c5e4e4693c0bce93ab4
sewerus <seweryn606@gmail.com>
2016-05-19 17:13:31 +0200

    AddImg #1

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@
 ActiveRecord::Schema.define(version: 20160519142439) do
 
   # These are extensions that must be enabled in order to support this database
+  #
   enable_extension "plpgsql"
 
   create_table "contacts", force: :cascade do |t|
